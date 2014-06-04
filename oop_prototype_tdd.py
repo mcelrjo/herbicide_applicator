ac866b6e1a40499a802ce831d3c81af0ebb6f5df
Scott McElroy <mcelroy@auburn.edu>
2014-06-04 14:25:28 -0500

    Finally got one unittest to pass

--- oop_prototype_tdd.py
+++ oop_prototype_tdd.py
@@ -4,12 +4,14 @@ from oop_prototype_classes import Herbicide
 
 class TddforHerbicideProject(unittest.TestCase):
 
-	def setUp(self):
-		self.herbs = Herbicide()
-		#self.pre = Preherbs()
+	#def setUp(self):
+	#	self.herbs = Herbicide()
+	#	#self.pre = Preherbs()
 
 	def test_string_entered_for_conc_value(self):
-		x = self.herbs.calc_formulation_rate("Revolver", 0.19, 0.011875, 90)
+		self.herbs = Herbicide("Revolver", 0.19, 0.011875, 9)
+		self.herbs.concUnits('liquid')
+		x = self.herbs.calc_formulation_rate()
 		#result = x.calc_formulation_rate()
-		self.assertEqual(0.062,x)
+		self.assertEqual(0.0625,x)
 	
